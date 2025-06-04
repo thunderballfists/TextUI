53941f2873a7e6848214d0344053c440163a173a
thunderballfists <130205658+thunderballfists@users.noreply.github.com>
2025-06-04 10:46:31 -0500

    Add assertion to validate_css test

--- tests/test_validate_css.py
+++ tests/test_validate_css.py
@@ -1,4 +1,9 @@
-from validate_css import validate_css
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from textui.validate_css import validate_css
 
 import logging
 
@@ -36,9 +41,12 @@ foo {
 }
 """
 
+expected = validate_css(css)
+
 
 def test_validate_css():
     logging.getLogger().setLevel(logging.DEBUG)
     output = validate_css(css)
 
     logging.debug(output)
+    assert output == expected
